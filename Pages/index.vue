88dd26e7e173934d6d67f6aed9ccec0f6ad0fb17
Kanishka <k.malviya@astoundcommerce.com>
2023-07-06 11:26:27 +0530

    Initial Commit

---
+++ Pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, iure? Dolorum, temporibus! Ad sed ipsa dolorum cumque, soluta voluptatem fuga culpa, unde ratione amet fugit facilis dignissimos animi atque corporis.
+        </p>
+    </div>
+</template>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+
+</style>
